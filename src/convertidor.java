251549f44d1870d5badb5b4258185344a21a61c2
gerardo <gerardoguillermolopezherrera@gmail.com>
2026-06-03 13:52:50 -0600

    proyecto inicial

---
+++ src/convertidor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class convertidor {
+    
+
+    public double millasAKilometros(double millas) {
+        return millas * 1.60934;
+    }
+
+    public double kilometrosAMillas(double kilometros) {
+        return kilometros / 1.60934;
+    }
+
+}
+    
+
